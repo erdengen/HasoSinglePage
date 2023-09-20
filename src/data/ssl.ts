ce3e6338263854fb7b5813e765d5d069178812ca
Erden Gencer <erden.gen@gmail.com>
2023-09-20 18:21:24 +0300

    change SSL data

--- src/data/ssl.ts
+++ src/data/ssl.ts
@@ -1,4 +1,4 @@
-export const sslData = [
+export const sslDataYedek = [
     {
         ids:'01',
         title:'HASO (Hemen Al Sonra Öde) Nedir?',
@@ -14,4 +14,179 @@ export const sslData = [
         title: "Bana Özel Hemen Al Sonra Öde limitim nasıl belirlenir?",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
     }
-]
+];
+export const sslData = [
+    {
+      ids: 8,
+      title: "Alışveriş anında otomatik ödeme talimatı verdiğim kredi / banka kartımda yeterli bakiyem olmasa bile",
+      desc: "Alışveriş esnasında kredi kartında yeterli miktarda ya da banka kartına bağlı hesabında yeterli bakiyen bulunmasa bile ihtiyaçlarını ertelemeden hızla ve kolayca alışverişini tamamlayabilirsin.",
+      
+    },
+    {
+      ids: 3,
+      title: "Bana özel Hemen Al Sonra Öde limiti nedir, nasıl öğrenirim?",
+      desc: "Hemen Al Sonra Öde limitin, HASO tarafından tanımlanan, ödeyebileceğin toplam alışveriş tutarındır. Limit saniyeler içinde belirlenir. Limitini öğrenmek için; *Hemen Al Sonra Öde butonunu tıkla *Açılan ekrana adın, soyadın, T.C. kimlik numaran, doğum tarihin ve bankacılık sisteminde kayıtlı telefon numaranı gir. ‘’HASO Kullanım Koşulları’ nı okudum, onaylıyorum’’ butonunu tıkla. *Kredi Kayıt Bürosu’nda (KKB) kredi notunu alabilmemiz için cep telefonuna ‘’HASO’’ (Hemen Al Sonra Öde) adına SMS ile gönderilen şifreyi ekrana gir.",
+      
+    },
+    {
+      ids: 4,
+      title: "Bana Özel Hemen Al Sonra Öde limitim nasıl belirlenir?",
+      desc: "Sana özel limitin; kişisel bilgilerin, HASO’ dan yaptığın alışverişler ve Kredi Kayıt Bürosu’ndan alacağımız bilgilerle finansal performansın değerlendirilerek otomatik belirlenir. Hemen Al Sonra Öde limitini oluşturmak Kredi Kayıt Bürosu skorunu etkilemez.",
+      
+    },
+    {
+      ids: 29,
+      title: "Bize Ulaşın adımı altında yaptığım çağrılar ücretsiz midir?",
+      desc: "Bize ulaşın adımı altında yaptığın çağrılar operatörünün uyguladığı tarifeye göre  ücretlendirilecektir.",
+      
+    },
+    {
+      ids: 26,
+      title: "Farklı kredi kartı ile nasıl ödeme yaparım?",
+      desc: "Ödemeler kategorisinde, ‘’ödeme yap’’ butonunu tıkladığında, ‘’Ödeme Yöntemi Ekle/Değiştir’’ başlığı altında, dilediğin kartını ekleyebilir veya değiştirebilirsin. Talebine göre kartını kaydedebilir veya kaydetmeden bir kereliğe mahsus da istediğin karttan ödemeni gerçekleştirebilirsin.",
+      
+    },
+    {
+      ids: 9,
+      title: "Findeks nedir?",
+      desc: "Findeks, kişilerin kredi notunun yanında kredi, kredi kartı ve kredili mevduat hesabı borçlarını ödeme alışkanlıkları, borç durumu, yeni kredili ürünleri ve kredi kullanım yoğunluğu da içeren detaylı bir rapordur. Findeks.com adresinden bu bilgilere ulaşılabilir.",
+      
+    },
+    {
+      ids: 2,
+      title: "HASO (Hemen Al Sonra Öde) Nedir?",
+      desc: "Kredi kartın olsun ya da olmasın, sana özel tanımlanan hazır limit ile alışveriş yapabileceğin yeni nesil bir alışveriş yöntemidir. HASO Platformu sayesinde ihtiyaçlarınızı taksitle HEMEN satın alıp, SONRA ödemeye başlarsınız. HASO (Hemen Al Sonra Öde) üyesi olan iş yerlerinin kendi müşterilerine taksitli satış yapmalarını sağlayan bir platformdur. HASO Platformu bir finansal kuruluş değildir sizi kredilendirmez, HASO ile yapmış olduğunuz tüm taksitlendirmeler ürünü satan HASO üye iş yerleri tarafından yapılmaktadır.",
+      
+    },
+    {
+      ids: 24,
+      title: "HASO ile kaç sipariş verebilirim?",
+      desc: "HASO ile henüz ilk kez alışveriş yapacaksan, ilk kayıt esnasında tanımlanan limit dahilinde sipariş verebilirsin. HASO ile alışveriş sıklığın ve siparişlerinin başarılı bir şekilde geri ödemen göz önüne alınarak, harcama olasılığın yükselir.",
+      
+    },
+    {
+      ids: 21,
+      title: "HASO’yu kullanmak için minimum yaş şartı var mı?",
+      desc: "HASO‘ yu yalnızca 18 yaş ve üzerindeki müşteriler kullanabilir.",
+     
+    },
+    {
+      ids: 20,
+      title: "Hemen Al Sonra Öde ile aldığım ürünleri iade ettim ama ödeme planında ödeme gözüküyor. Limitim de ge",
+      desc: "İade sürecin devam ediyor olabilir. Konuyla ilgili üye işyeri ile iletişime geçip, iade sürecinin tamamlandığına emin ol.",
+     
+    },
+    {
+      ids: 15,
+      title: "Hemen Al Sonra Öde ile aldığım ürünleri iptal veya iade edersem ne olur?",
+      desc: "Ödeme günün gelmeden siparişini iptal / iade edersen bu ürünler için bir ödeme yapmazsın, iptal veya iade edilen ürünlerin tutarları, vadesi geldiğinde ödenecek toplam tutardan düşülür.Siparişin tamamını iptal edersen, 1 ay ertleme için ödediğin işlem ücretin  iade edilir, ancak siparişin tamamı iptal edilmez ise işlem ücreti de iade edilmez.",
+     
+    },
+    {
+      ids: 7,
+      title: "Hemen Al Sonra Öde ile taksitli alışveriş yapabilir miyim?",
+      desc: "Hemen Al Sonra Öde, ürün grubu veya sipariş tutarına göre değişen taksit imkanı sunar. İlk taksitini seçtiğin kanaldan 1 ay sonra ödersin. Otomatik ödeme talimatı verdiysen, her ay ödeme tarihinde seçtiğin kartında taksit tutarı kadar limit olduğundan emin olmalısın.",
+      
+    },
+    {
+      ids: 11,
+      title: "Hemen Al Sonra Öde limitimi sorguladım ama kullanmadım. Benden herhangi bir ücret alınır mı?",
+      desc: "Hayır, hiçbir ücret alınmaz.",
+     
+    },
+    {
+      ids: 14,
+      title: "Hemen Al Sonra Öde limitimi sorguladım.Benden habersiz hesabımdan bir başkası alışveriş yapabilir mi",
+      desc: "Hayır, limitini öğrendikten sonra kimse senin yerine alışveriş yapamaz; her Hemen Al Sonra Öde siparişi verdiğinde SMS ile şifre göndererek siparişin sana ait olup olmadığını teyit etmeni istiyoruz.",
+      
+    },
+    {
+      ids: 17,
+      title: "Hemen Al Sonra Öde seçeneğini her bankanın kartlarıyla kullanabilir miyim?",
+      
+    },
+    {
+      ids: 25,
+      title: "İadelerimi nereden takip edebilirim?",
+      desc: "İade talebinde bulunduğun marka ile iletişime geçerek, iade işlemini takip edebilirsin.",
+     
+    },
+    {
+      ids: 6,
+      title: "Kredi Kartı ile taksitli alışveriş ve Hemen Al Sonra Öde ile taksitli alışveriş arasındaki fark nedi",
+      desc: "Kredi kartı ile taksitli alışverişlerde taksitlendirme banka tarafından yapılır ve alışveriş tutarının tamamı kart limitinden anında düşer. Hemen Al Sonra ile yapılan taksitli alışverişler, Kredi kartı limitini bloke etmez. Hemen Al Sonra Öde ile yapılan taksitli alışverişlerde ise ilk ödeme bir ay sonra alınır ve her taksit ödeme gününde sadece taksit tutarı kadar kartından (kart ile ödeme talimatı verdiysen) çekim yapılır. Dilersen diğer ödeme kanallarından (Havale/ EFT, Ptt, Mobil Ödeme, Debit Kart) da ödemeni gerçekleştirebilirsin.",
+    
+    },
+    {
+      ids: 5,
+      title: "Limit Tanımlama Talebimde miktar neye göre belirleniyor?",
+      desc: "Limit talebinizde bir dizi faktörü dikkate alırız. Finansal geçmişin, HASO ile alışveriş sıklığın ve ne kadar çok siparişi başarılı bir şekilde, gününde geri ödediğin vb gibi faktörler doğrultusunda, tarafına bir limit tanımlanır.",
+      
+    },
+    {
+      ids: 30,
+      title: "Müşteri Hizmetlerine ulaşamıyorum, ne yapmalıyım?",
+      desc: "HASO Müşteri Hizmetleri 09:00-18:00 saatleri arasında hizet vermektedir. Arayıp ulaşamama durumunuzda, mesai saatleri içerisinde sizlere tekrar dönüş sağlanıyor olunacaktır.",
+      
+    },
+    {
+      ids: 13,
+      title: "Ödeme aşamasında Hemen Al Sonra Öde seçeneği çıkmıyor. Ne yapabilirim?",
+      desc: "Hemen Al Sonra ile ödeme yapabilmek için kayıtlı bir banka / kredi kartın olduğundan emin ol.",
+      
+    },
+    {
+      ids: 22,
+      title: "Ödeme zamanı geldiğinde bilgilendirilecek miyim?",
+      desc: "HASO ile alışverişinizi tamamlandığın anda öncelikle tarafına, bir ay sonra başlayacak ödeme planın mail ile iletilir. Ayrıca ödeme gününden 2 gün önce ve ödeme gününden 2 gün sonra(henüz ödeme gerçekleştirmediysen) içinde ödeme linklerinin de yer aldığı sms ile tarafına hatırlatma yapılır.",
+     
+    },
+    {
+      ids: 19,
+      title: "Ödememi geciktirirsem ne olur?",
+      desc: "Günü gelen ödemelerini alamazsak sana SMS, e-posta ve arama ile bilgilendirme yapacağız. Yapılan hatırlatmalara rağmen ödeme yine alınamazsa 90 gün sonrası ne yazık ki idari ve yasal borç takip süreci başlatılır.",
+      
+    },
+    {
+      ids: 18,
+      title: "Ödemenin bir kısmını peşin, bir kısmını taksitle yapabilir miyim?",
+      desc: "Evet, tek çekim ya da taksitli ödeme seçeneği ile alışverişini tamamlayabilirsin.",
+      
+    },
+    {
+      ids: 16,
+      title: "Ödemeyi nasıl yaparım?",
+      desc: "HASO ile alışverişini tamamladıktan sonra taksit planını ve bu plana ait ödeme linklerini e-posta ile tarafına iletiriz. Aynı zamanda vade tarihinden 2 gün önce SMS ile ödeme linkini ileterek hatırlatma yaparız. Dilersen ilettiğimiz ödeme linkine tıklayarak veya HASO Aplikasyonu üzerinden ödemelerini gerçekleştirebilirsiniz.  Kredi Kartı ile Ödeme İçin; Alışveriş esnasında, Masterpass veya Visa Tek Tıkla alt yapısında kayıtlı olan ve  ödemenin çekileceği kartlarından birini seçerek veya dilersen yeni kart ekleyerek ‘’Taksit Ödeme gününde seçili olan kartımdan otomatik çekilmesine onay veriyorum’’ butonuna tıklayarak; talimatı onaylayabilirsin. Böylece ödemen her ay seçtiğin kredi kartından otomatik olarak çekilir. Ödeme yapılana kadar alışveriş tutarın kart limitinden düşmez. Sipariş verdikten sonra dilersen ödeme yapacağın kartı değiştirebilirsin. Havale / EFT, PTT, Mobil Ödeme İçin, Kartlı Ödeme Haricinde Bir Yöntem ile Ödeme Yapacağım butonuna tıklayarak, ödeme gününde dilediğin kanal ile ödemeni gerçekleştirebilirsin.",
+      
+    },
+    {
+      ids: 28,
+      title: "QR Kodu Nasıl/Nerede Kullanabilirim?",
+      desc: "Aplikasyon üzerinde QR Kod oluştur butonuna tıkladığında, HASO’ nun geçerli olduğu mağazalar özelinde, QR Kodunu okutarak alışveriş yapabilirsin.",
+      
+    },
+    {
+      ids: 23,
+      title: "Siparişim neden reddediliyor?",
+      desc: "HASO, siparişlerin onayını belirlemek için çeşitli faktörleri kullanır. Sipariş miktarı, limitini aşıyor ise reddedilmesi olasıdır. Bireysel limitine ek olarak, alışveriş yaptığın markaya bağlı ek sınırlamalar olabilir. Sipariş tutarı limitini aşıyor ise bir kısmı peşin ödeme seçeneği ile geri kalan ödemeni taksitlendirebilirsin.",
+      
+    },
+    {
+      ids: 10,
+      title: "Telefon numaramı doğrulamak adımında ‘’Doğrulama kodu gönder’’ dedim ama doğrulama kodu SMS’i gelmed",
+      desc: "Kullandığın GSM operatöründen kaynaklı bir sorun yaşıyor olabilirsin veya telefonunda Findeks numarası bloklanmış olabilir.",
+      
+    },
+    {
+      ids: 12,
+      title: "Telefonuma gelen sms şifreyi bir başkası kullanabilir mi?",
+      desc: "Telefonunuza gelen sms şifreyi kimseyle paylaşmamalısınız. Aksi takdirde sizi borçlandırabilirler.",
+     
+    },
+    {
+      ids: 27,
+      title: "Vadesi geçen ödemem olduğunda, HASO ile alışverişe devam edebilir miyim?",
+      desc: "HASO ile yaptığın alışverişlerde, vadesi geçen ödemelerin olduğu takdirde, limitin olsa da alışverişe devam edemezsin.",
+      
+    }
+  ];
